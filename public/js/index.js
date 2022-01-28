f69a4e576e4abeadd9ce0040bee00fb92081a4ec
Yasin <mostafa.a.yasin@gmail.com>
2022-01-28 21:00:39 +0200

    minor fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
     async function search(q, sortBy, order) {
-        let result = await fetch(`/api/v1/search?q=${q}&sortBy=${sortBy}&order=${order}`)
+        let result = await fetch(`/api/v1/search?q=${q}&sort_by=${sortBy}&order=${order}`)
             .then(response => response.json());
         console.log(result);
 
